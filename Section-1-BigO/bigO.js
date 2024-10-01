fec652148e95f582bc97fc707633085ce8cb3566
Freshizy <mohgli93@gmail.com>
2024-09-30 22:04:49 -0400

    added notes for log2 and different terms for inputs

--- Section-1-BigO/bigO.js
+++ Section-1-BigO/bigO.js
@@ -82,12 +82,156 @@ function nSquared(n){
 
 function nSquared(n){
     for(let i = 0; i < n; i++){
-        for(let j =0; j < 0; j++){
+        for(let j =0; j < n; j++){
             console.log(i,j)
         }
     }
 }
-nSquared(10)
+//nSquared(10)
+
+/*
+
+This outputs 100 items
+0 0 
+0 1
+0 2 
+0 3 .... and so on up to 
+9 9 
 
 
+the number of items that were output in this case is:
 
+n * n or n^2
+
+0(n^2) will execute operations quicker than O(n)
+*/
+
+function dropNonDominants(n){
+    for(let i = 0; i < n; i++){
+        for(let j =0; j < n; j++){
+            console.log(i,j)
+        }
+    }
+    for(let k =0; k < n; k++){
+        console.log(k)
+    }
+}
+
+/*
+the nested loop ran o(n^2) times and the 2nd loop ran o(n) times. 
+
+when added together:
+
+0(n^2 + n)
+imagine n being 100
+n^2 = 1000 (dominant)
+n = 100 (nondominant)
+
+drop the nondominant
+*/
+
+/*   3: 0(1) 
+
+it is the most efficient bigO notation and it is referred to as constant time
+
+below in the function OofOne:
+no matter what n is, the number of operations is constant.
+*/
+function OofOne(n){
+    return n + n + n
+}
+
+
+/*   4: 0(log n)
+works with best with sorted data
+
+want to get to 1 from  array 
+
+divide and conquer:
+
+[1,2,3,4,5,6,7,8]
+
+1:
+[1,2,3,4] [5,6,7,8]
+
+2:
+[1,2] [3,4]
+
+3:
+[1,2]
+
+[1]
+
+log sub 2 of 8 equals 3
+log2(8) = 3
+
+2 to the what power equals 8? 
+3
+
+means 8 needed to split three times to get to 1 
+
+imagine working with data much larger in the millions.
+this method of using O(log n) can help reach our solutions quicker 
+*/
+
+
+/*   5: different terms for inputs
+is a very popular technical interview question 
+
+consider the following function that takes n into the function and has 2 loops
+*/
+
+function sameTermsForInputs(n){
+    for(let i = 0; i < n; i++){
+        console.log(i)
+    }
+    for(let j = 0; j < n; j++){
+        console.log(j)
+    }
+}
+ /*
+ each of these loops ran n times : n + n = 0(2n)
+ we drop the constant and make it 0(n)
+ */
+
+
+ function diffTermsForInputs(a,b){
+    for(let i = 0; i < a; i++){
+        console.log(i)
+    }
+    for(let j = 0; j < b; j++){
+        console.log(j)
+    }
+}
+
+ /*
+ both are single for-loops aka 0(n)
+ oen would assume that it is :
+ 0(n) + 0(n) = 0(2n).
+ to than drop the constant to make it 0)n
+
+
+ this is incorrect because we cannot assume that a and b are both equal to n.
+ cannot have both variables equal to n. a can be 1 and b can be 10.
+ each loop will return a very different result.
+ therefore we must assume this expression instead:
+ 
+ 0(a) + 0(b) = 0(a+b)
+
+
+      0(a+b) is as far as we can simplify this 
+
+the nested function below will equate to a a similar expression 
+ */
+
+function timesDiffTermsForInputs(a,b){
+    for(let i = 0; i < a; i++){
+        for(let j = 0; j < b; j++){
+            console.log (a,b)
+        }
+    }
+}
+
+/*
+it will be O(a*b)
+*/
--- Section-1-BigO/tempCodeRunnerFile.js
+++ Section-1-BigO/tempCodeRunnerFile.js
@@ -1,9 +1,1 @@
-function nSquared(n){
-    for(let i = 0; i < n; i++){
-        for(let j =0; j < 0; j++){
-            console.log(i,j)
-        }
-    }
-
-}
-nSquared(10)
+//dropConstants(3)
